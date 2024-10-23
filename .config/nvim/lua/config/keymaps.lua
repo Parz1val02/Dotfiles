897df73fdb6043404428d81cc00fc6761722a404
r.b4rr10s <rodrigoedu11@gmail.com>
2024-10-23 09:40:02 -0500

    no c

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,17 +1,17 @@
 -- Directory Navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', {noremap = true, silent = true })
-vim.keymap.set('n', '<C-l>', '<C-w>l', {noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 --Set keymap for vertical motions, center half page up and half page down
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
 --Center the keyword while searching
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 --Move around highlighted lines in visual mode
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 --Cursor does not move from position when appending below to the end of the current lines
-vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set("n", "J", "mzJ`z")
 -- Indenting
 vim.keymap.set("v", "<", "<gv") -- Shift Indentation to Left
 vim.keymap.set("v", ">", ">gv") -- Shift Indentation to Right
@@ -19,4 +19,5 @@ vim.keymap.set("v", ">", ">gv") -- Shift Indentation to Right
 vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
 --Set keymap to open Git
-vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = 'Open Git page' })
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Open Git page" })
+
